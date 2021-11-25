b0dd3a167f0dc5ddd273485b684249a773d1f78c
robertocarlosmedina <robertocarlosmedina.dev@gmail.com>
2021-11-25 16:00:41 -0100

    Code refresh

--- app/src/main/java/com/example/masrapt/MainActivity.java
+++ app/src/main/java/com/example/masrapt/MainActivity.java
@@ -10,19 +10,18 @@ import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     Timer timer;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //timer = new Timer();
-        //timer.schedule(new TimerTask() {
-        //    @Override
-        //  public void run() {
-        //      Intent intent = new Intent(MainActivity.this, Dashboard_activity.class);
-        //      startActivity(intent);
-        //      finish();
-        //  }
-        //}, 5000);
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+        @Override
+        public void run() {
+              Intent intent = new Intent(MainActivity.this, Dashboard_activity.class);
+              startActivity(intent);
+              finish();
+          }
+        }, 0);
     }
 }
